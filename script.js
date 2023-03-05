7fdc2508fac9902adbd44b879c4ecf989139cb61
Holerion <qwere111133334444@gmail.com>
2023-03-05 22:31:00 +0200

    style

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-location.reload(true);
+const heder = document.querySelector('nav')
+let prevHeight = scrollY;
+
+
+const hederOut=(heder)=>{
+    heder.classList.remove("hadeIn")
+    heder.classList.add("hadeOut")
+  }
+  const hederIn=(heder)=>{
+    heder.classList.remove("hadeOut")
+  heder.classList.add("hadeIn")
+}
+
+const slideBG = (herBG) => {
+    herBG.style.backgroundPosition = scrollY / 25 + 10 + "%";
+  };
+
+
+  window.addEventListener("scroll", function () {
+    slideBG(document.querySelector(".hero"));
+
+if (prevHeight>scrollY) {
+    hederIn(heder)
+    
+    prevHeight=scrollY
+}else{
+    hederOut(heder)
+
+    prevHeight=scrollY
+}
+
+  });
